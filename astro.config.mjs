ffe466beff881ed967adbe7d1e40f8e66fd390fb
Nathaniel Tampus <nate@mugnavo.com>
2024-12-03 20:39:25 +0800

    astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mugnavo.com",
   integrations: [sitemap(), tailwind({ applyBaseStyles: false }), solid()],
-  output: "hybrid",
-  adapter: vercel({ functionPerRoute: false }),
-  experimental: {
-    directRenderScript: true,
-  },
+  output: "static",
+  adapter: vercel(),
 });
