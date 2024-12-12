a7bd877f84b64b87c7fe08667cf51a717d1734bc
Nathaniel Tampus <nate@mugnavo.com>
2024-12-12 18:08:16 +0800

    add temporary sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import { sitemapFix } from "./temp-sitemap-fix";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mugnavo.com",
-  integrations: [sitemap(), tailwind({ applyBaseStyles: false }), solid()],
+  integrations: [
+    sitemap(),
+    tailwind({ applyBaseStyles: false }),
+    solid(),
+    sitemapFix(), // TODO: temporary fix: https://github.com/withastro/adapters/issues/445#issuecomment-2526327882
+  ],
   output: "static",
   adapter: vercel(),
 });
